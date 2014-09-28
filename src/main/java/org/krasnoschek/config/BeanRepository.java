76d69784cc11408493964a688d34ade6964f4401
alexander <a.krasnoschek@gmail.com>
2014-09-28 23:41:01 +0300

    New changelist

---
+++ src/main/java/org/krasnoschek/config/BeanRepository.java
@@ -0,0 +1,14 @@
+package org.krasnoschek.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanRepository {
+
+   /* @Bean
+    public MongoTemplate mongoTemplate() throws UnknownHostException {
+        MongoClient mongo = new MongoClient("localhost");
+        return new MongoTemplate(mongo, "site");
+    }*/
+
+}
---
+++ src/main/java/org/krasnoschek/dao/UserDao.java
@@ -0,0 +1,26 @@
+package org.krasnoschek.dao;
+
+
+
+import org.krasnoschek.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+//@Autowired
+//    private MongoTemplate mongoTemplate;
+
+
+    List<User> getUsers(){
+
+//        mongoTemplate.
+
+        return null;
+
+    }
+
+}
